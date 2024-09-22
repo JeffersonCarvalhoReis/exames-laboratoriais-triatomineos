99141ed6e7791cb133903487a647bf5930b0c8ba
Jefferon Carvalho <jeffersoncarvalhodosreis@gmail.com>
2024-09-22 03:43:53 -0300

    firebase correto adicionado

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,18 +1,20 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// Import the functions you need from the SDKs you need
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDtgweeyJwsrIN_xTAE1z5-1vSeDY7SKyw",
-  authDomain: "triatomineos-teste.firebaseapp.com",
-  projectId: "triatomineos-teste",
-  storageBucket: "triatomineos-teste.appspot.com",
-  messagingSenderId: "461735364848",
-  appId: "1:461735364848:web:ef4d03e5bc1ad54a2c5a35",
+  apiKey: "AIzaSyARoXOyT_lMgVPtmJHX-JwIBxKFfyKZcCs",
+  authDomain: "triatomineos-69e8c.firebaseapp.com",
+  projectId: "triatomineos-69e8c",
+  storageBucket: "triatomineos-69e8c.appspot.com",
+  messagingSenderId: "313793497472",
+  appId: "1:313793497472:web:998a7289d9f38a2d6f1965",
+  measurementId: "G-YLSTW3P1MQ",
 };
 
 // Initialize Firebase
